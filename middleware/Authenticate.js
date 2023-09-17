c35e9f8c505fe9e9118a2a001984f9d76455a0d0
AayushDabral <aayush05dabral@gmail.com>
2023-09-18 01:05:31 +0530

    Trying no cookie change

--- middleware/Authenticate.js
+++ middleware/Authenticate.js
@@ -7,6 +7,10 @@ const Authenticate = async (req, res, next) => {
         // const token = req.headers.cookie.split("=")[1]; //Req from postman
         // const token = req.cookie.split("jwtoken=")[1]; //Req for browser
 
+        if(!req.headers.cookie){
+            return res.status(200).send("No token provided")
+        }
+
         const cookieSegments = req.headers.cookie.split('; ');
 
         // Initialize a variable to store the token value
